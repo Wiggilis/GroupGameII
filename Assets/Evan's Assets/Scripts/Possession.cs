1cd76e75f1ef05ef48cdb286674ed3c10aa75908
etomsky <etomsy@yahoo.com>
2020-02-11 12:28:05 -0500

    Tags
    Added some tags

--- Assets/Evan's Assets/Scripts/Possession.cs
+++ Assets/Evan's Assets/Scripts/Possession.cs
@@ -4,15 +4,69 @@ using UnityEngine;
 
 public class Possession : MonoBehaviour
 {
+    public Rigidbody sphereRigidbody;
+    public float forcesPower;
+    private bool Wispress = false;
+    private bool Aispress = false;
+    private bool Sispress = false;
+    private bool Dispress = false;
+
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
+    void FixedUpdate()
+    {
+        if (Wispress == true)
+        {
+            sphereRigidbody.AddForce(Vector3.forward * forcesPower);
+            Wispress = false;
+        }
+        if (Sispress == true)
+        {
+            sphereRigidbody.AddForce(Vector3.back * forcesPower);
+            Sispress = false;
+        }
+        if (Aispress == true)
+        {
+            sphereRigidbody.AddForce(Vector3.left * forcesPower);
+            Aispress = false;
+        }
+        if (Dispress == true)
+        {
+            sphereRigidbody.AddForce(Vector3.right * forcesPower);
+            Dispress = false;
+        }
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.W))
+        {
+            Wispress = true;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Sispress = true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Aispress = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Dispress = true;
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("wall"))
+        {
+            GetComponent<Collider>().enabled = false;
+        }
     }
 }
