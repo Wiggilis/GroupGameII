d0d2480bcaaa9c8295d226944c66cacdfebf2382
AngeloSanchez116 <56123283+AngeloSanchez116@users.noreply.github.com>
2020-02-13 12:21:46 -0500

    the last changes didnt work for some reason

---
+++ Assets/Angelo_s Asset/Scripts/PlayerPossesiom.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPossesiom : MonoBehaviour
+{
+    public MeshRenderer playerMR;
+    public GameObject playerMovement;
+    public GameObject humanpossesion1;
+    public GameObject walls;
+    
+  
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray toMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit rhinfo;
+        bool didgit = Physics.Raycast(toMouse, out rhinfo, 500.0f);
+
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (rhinfo.collider.name == "Human") {
+                playerMR.enabled = false;
+                playerMovement.GetComponent<PlayerMovement>().enabled = false;
+                humanpossesion1.GetComponent<Possession>().humanpossision = true;
+                playerMovement.GetComponent<WalktoWall>();
+                
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            if (rhinfo.collider.name != "Human") {
+                playerMR.enabled = true;
+                playerMovement.GetComponent<PlayerMovement>().enabled = true;
+                humanpossesion1.GetComponent<Possession>().humanpossision = false;
+            }
+        }
+
+    }
+}
---
+++ Assets/Angelo_s Asset/Scripts/WalktoWall.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalktoWall : MonoBehaviour
+{
+    public Rigidbody playerRb;
+    public GameObject player;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+    void OnCollisionEnter(Collision Collision)
+    {
+        if (Collision.gameObject.tag == "Wall")
+        {
+            Collision.gameObject.GetComponent<BoxCollider>().enabled = false;
+        }
+    }
+
+
+
+}
