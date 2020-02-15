b0b90f5e99ecc2ed144420f80122f5a3c3207496
AngeloSanchez116 <56123283+AngeloSanchez116@users.noreply.github.com>
2020-02-15 12:37:49 -0500

    New Assest & New Script

---
+++ Assets/Angelo_s Asset/Scripts/GameController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+  
+    public GameObject humanCandleRef;
+    public bool fuctionwascalled = false;
+    int counter;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (fuctionwascalled == true) {
+            Addcounter();
+        }
+        
+    }
+
+    void Addcounter() {
+
+        counter++;
+        print(counter);
+        fuctionwascalled = false;
+    
+    }
+}
---
+++ Assets/Angelo_s Asset/Scripts/HumanCandleScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanCandleScript : MonoBehaviour
+{
+    public GameObject GameControllerRef;
+   
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "GameObject") {
+            Destroy(collision.gameObject);
+            GameControllerRef.GetComponent<GameController>().fuctionwascalled = true;
+        }   
+    }
+}
