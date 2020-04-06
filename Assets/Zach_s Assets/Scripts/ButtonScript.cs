c1404943b3c3d001ae8b50ed833e114022496bcf
Zachary Kolibas <37418284+Wiggilis@users.noreply.github.com>
2020-04-06 01:31:05 -0400

    House Beautification

---
+++ Assets/Zach_s Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScript : MonoBehaviour
+{
+    public void playButton(string SceneToLoad)
+    {
+        SceneManager.LoadScene(SceneToLoad);
+    }
+}
---
+++ Assets/Zach_s Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    private Image progressBar1;
+    [SerializeField]
+    private Image progressBar2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(LoadSceneOp());
+    }
+
+    // Update is called once per frame
+    IEnumerator LoadSceneOp()
+    {
+        AsyncOperation LevelProgress = SceneManager.LoadSceneAsync("Angelo_s Scene");
+        
+        while (LevelProgress.progress < 1)
+        {
+            progressBar1.fillAmount = LevelProgress.progress;
+            progressBar2.fillAmount = LevelProgress.progress;
+            yield return new WaitForEndOfFrame();
+        }
+
+    }
+}
