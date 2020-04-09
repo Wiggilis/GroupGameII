2a1a9a2e14c45be7467f1ccc2be923107c8a9792
Zachary Kolibas <37418284+Wiggilis@users.noreply.github.com>
2020-04-09 18:19:11 -0400

    Light Fixes and minor tweaks

---
+++ Assets/Zach_s Assets/Scripts/RotateScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateScript : MonoBehaviour
+{
+    public int speed;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, speed, 0);
+    }
+}
